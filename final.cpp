05072ce07f8e5e97504d13565dc418f6bdb1a056
interestingDev2 <74779333+interestingDev2@users.noreply.github.com>
2020-11-20 21:40:01 +0300

    Update final.cpp
    odev yardim computerengineer13

--- final.cpp
+++ final.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+// Odev yardim
 
 int main(int argc, char** argv) {
 	int a;
